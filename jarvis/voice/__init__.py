67f7f61d4b60b9fbb6e1df7b252480d2e151b95f
UnofficaI <walleyqyree@gmail.com>
2026-07-13 11:02:40 -0500

    Fix voice module imports - add VoiceAssistant to __init__.py

--- jarvis/voice/__init__.py
+++ jarvis/voice/__init__.py
@@ -2,5 +2,6 @@
 
 from .speech_recognition import VoiceRecognizer
 from .text_to_speech import TextToSpeech
+from .voice_assistant import VoiceAssistant
 
-__all__ = ["VoiceRecognizer", "TextToSpeech"]
+__all__ = ["VoiceRecognizer", "TextToSpeech", "VoiceAssistant"]
